aa2b26307c245acb6cc533d95773e0d90647023c
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-21 22:52:40 -0300

    feat: user model

---
+++ models/user.py
@@ -0,0 +1,16 @@
+from database import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    refeicoes = db.relationship('Refeicao', backref='user', lazy=True)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "refeicoes": [refeicao.to_dict() for refeicao in self.refeicoes] 
+        }
